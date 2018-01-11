d75a74eeb4e24b31916aaf61396accdf286746f6
Paul Highum <phigh07@gmail.com>
2018-01-11 15:00:19 -0700

    begins drill two, creates database, adds pg, knex, dotenv, creates migration coffee

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+require("dotenv").config()
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      filename: "postgres://localhost/coffee"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL
+  }
+}
